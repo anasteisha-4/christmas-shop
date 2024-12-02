8a874c3d85082a32f9c0579924ee9f53180e16f4
Bronina A.O <bronina04@mail.ru>
2024-12-03 01:12:58 +0300

    feat: add resize event listener for scroll script

--- christmas-shop/scripts/scroll-script.js
+++ christmas-shop/scripts/scroll-script.js
@@ -1,16 +1,27 @@
-if (document.documentElement.clientWidth <= 768) {
-  function toggleScrollArrow() {
-    if (window.scrollY >= 300) {
-      scrollArrowElement.classList.add('active');
-    } else {
-      scrollArrowElement.classList.remove('active');
-    }
-  }
+let scrollArrowElement = document.getElementById('scroll-arrow');
 
-  let scrollArrowElement = document.getElementById('scroll-arrow');
-  toggleScrollArrow();
-  window.addEventListener('scroll', toggleScrollArrow);
-  scrollArrowElement.addEventListener('click', () => {
-    window.scrollTo({ top: 0, right: 0, behavior: 'smooth' });
-  });
+function toggleScrollArrow() {
+  if (window.scrollY >= 300) {
+    scrollArrowElement.classList.add('active');
+  } else {
+    scrollArrowElement.classList.remove('active');
+  }
 }
+
+function setButton() {
+  if (document.documentElement.clientWidth <= 768) {
+    scrollArrowElement.style.display = '';
+    toggleScrollArrow();
+    window.addEventListener('scroll', toggleScrollArrow);
+    scrollArrowElement.addEventListener('click', () => {
+      window.scrollTo({ top: 0, right: 0, behavior: 'smooth' });
+    });
+  } else {
+    scrollArrowElement.style.display = 'none';
+  }
+}
+
+setButton();
+window.addEventListener('resize', () => {
+  setButton();
+});
